0877f3e73f3f85df2d7e42ae9390599476d93b6b
Eric Dahl <eric.c.dahl@gmail.com>
2017-04-29 21:40:38 -0700

    switch to amazon linux

--- main.tf
+++ main.tf
@@ -50,39 +50,6 @@ resource "aws_autoscaling_group" "app" {
   launch_configuration = "${aws_launch_configuration.app.name}"
 }
 
-data "template_file" "cloud_config" {
-  template = "${file("${path.module}/cloud-config.yml")}"
-
-  vars {
-    aws_region         = "${var.aws_region}"
-    ecs_cluster_name   = "${aws_ecs_cluster.main.name}"
-    ecs_log_level      = "info"
-    ecs_agent_version  = "latest"
-    ecs_log_group_name = "${aws_cloudwatch_log_group.ecs.name}"
-  }
-}
-
-data "aws_ami" "stable_coreos" {
-  most_recent = true
-
-  filter {
-    name   = "description"
-    values = ["CoreOS stable *"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["595879546273"] # CoreOS
-}
-
 resource "aws_launch_configuration" "app" {
   security_groups = [
     "${aws_security_group.instance_sg.id}",
@@ -90,11 +57,16 @@ resource "aws_launch_configuration" "app" {
 
   key_name                    = "${var.key_name}"
 
-//  image_id= "ami-62d35c02"
-  image_id                    = "${data.aws_ami.stable_coreos.id}"
+  image_id= "ami-62d35c02"
   instance_type               = "${var.instance_type}"
+
   iam_instance_profile        = "${aws_iam_instance_profile.app.name}"
-  user_data                   = "${data.template_file.cloud_config.rendered}"
+  user_data = <<EOF
+#!/bin/bash
+
+echo 'ECS_CLUSTER=terraform_example_ecs_cluster' > /etc/ecs/ecs.config
+start ecs
+EOF
   associate_public_ip_address = true
 
   lifecycle {
