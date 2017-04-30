4295c8cf44ffc7b5614afd19ff2a64d0fc14077d
Eric Dahl <eric.c.dahl@gmail.com>
2017-04-29 20:24:41 -0700

    working nginx

--- main.tf
+++ main.tf
@@ -1,168 +1,327 @@
+# Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  tags {
-    Name = "terraform vpc"
+## EC2
+
+### Network
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count             = "${var.az_count}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
   }
 }
 
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+resource "aws_route_table_association" "a" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${aws_route_table.r.id}"
 }
 
-resource "aws_route" "internet_access" {
-  route_table_id = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.default.id}"
+### Compute
+
+resource "aws_autoscaling_group" "app" {
+  name                 = "tf-test-asg"
+  vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
+  min_size             = "${var.asg_min}"
+  max_size             = "${var.asg_max}"
+  desired_capacity     = "${var.asg_desired}"
+  launch_configuration = "${aws_launch_configuration.app.name}"
 }
 
-resource "aws_subnet" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
+data "template_file" "cloud_config" {
+  template = "${file("${path.module}/cloud-config.yml")}"
 
-resource "aws_security_group" "elb" {
-  name = "terraform_elb"
-  vpc_id = "${aws_vpc.default.id}"
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
+  vars {
+    aws_region         = "${var.aws_region}"
+    ecs_cluster_name   = "${aws_ecs_cluster.main.name}"
+    ecs_log_level      = "info"
+    ecs_agent_version  = "latest"
+    ecs_log_group_name = "${aws_cloudwatch_log_group.ecs.name}"
   }
 }
 
-resource "aws_security_group" "default" {
-  name = "terraform_sg"
-  vpc_id = "${aws_vpc.default.id}"
+data "aws_ami" "stable_coreos" {
+  most_recent = true
 
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "description"
+    values = ["CoreOS stable *"]
   }
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+
+  owners = ["595879546273"] # CoreOS
 }
 
-resource "aws_elb" "web" {
-  name = "terraform-example-elb"
-
-  subnets = [
-    "${aws_subnet.default.id}"]
+resource "aws_launch_configuration" "app" {
   security_groups = [
-    "${aws_security_group.elb.id}"]
-  instances = [
-    "${aws_instance.web.*.id}"]
+    "${aws_security_group.instance_sg.id}",
+  ]
 
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
+  key_name                    = "${var.key_name}"
 
-  health_check {
-    target = "HTTP:80/?lb_check"
-    interval = 10
-    timeout = 3
-    healthy_threshold = 3
-    unhealthy_threshold = 3
+//  image_id= "ami-62d35c02"
+  image_id                    = "${data.aws_ami.stable_coreos.id}"
+  instance_type               = "${var.instance_type}"
+  iam_instance_profile        = "${aws_iam_instance_profile.app.name}"
+  user_data                   = "${data.template_file.cloud_config.rendered}"
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
-//
-//  access_logs {
-//    bucket        = "${aws_s3_bucket.default.id}"
-//    bucket_prefix = "logs"
-//    interval      = 5
-//  }
-//}
-//
-//resource "aws_s3_bucket" "default" {
-//  bucket = "terraform-elb-logs"
-//
-//}
 
+### Security
 
-# Simply specify the family to find the latest ACTIVE revision in that family.
-//data "aws_ecs_task_definition" "mongo" {
-//  task_definition = "${aws_ecs_task_definition.mongo.family}"
-//}
+resource "aws_security_group" "lb_sg" {
+  description = "controls access to the application ELB"
 
-resource "aws_ecs_cluster" "default" {
-  name = "terraform-ecs-cluster"
-}
-//
-//resource "aws_ecs_task_definition" "mongo" {
-//  family = "mongodb"
-//
-//  container_definitions = <<DEFINITION
-//[
-//  {
-//    "cpu": 128,
-//    "environment": [{
-//      "name": "SECRET",
-//      "value": "KEY"
-//    }],
-//    "essential": true,
-//    "image": "mongo:latest",
-//    "memory": 128,
-//    "memoryReservation": 64,
-//    "name": "mongodb"
-//  }
-//]
-//DEFINITION
-//}
-//
-//resource "aws_ecs_service" "mongo" {
-//  name          = "mongo"
-//  cluster       = "${aws_ecs_cluster.foo.id}"
-//  desired_count = 2
-//
-//  # Track the latest ACTIVE revision
-//  task_definition = "${aws_ecs_task_definition.mongo.family}:${max("${aws_ecs_task_definition.mongo.revision}", "${data.aws_ecs_task_definition.mongo.revision}")}"
-//}
+  vpc_id = "${aws_vpc.main.id}"
+  name   = "tf-ecs-lbsg"
 
-
-resource "aws_instance" "web" {
-  ami = "ami-d9a33ab9"
-  instance_type = "t2.micro"
-  key_name = "ec2-temp-2017-04-21"
-  subnet_id = "${aws_subnet.default.id}"
-
-  connection {
-    user = "ec2-user"
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
 
-  count = 2
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
 
+resource "aws_security_group" "instance_sg" {
+  description = "controls direct access to application instances"
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "tf-ecs-instsg"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+
+    cidr_blocks = [
+      "${var.admin_cidr_ingress}",
+    ]
+  }
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 8080
+    to_port   = 8080
+
+    security_groups = [
+      "${aws_security_group.lb_sg.id}",
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+## ECS
+
+resource "aws_ecs_cluster" "main" {
+  name = "terraform_example_ecs_cluster"
+}
+
+data "template_file" "task_definition" {
+  template = "${file("${path.module}/task-definition.json")}"
+
+  vars {
+//    image_url        = "ghost:latest"
+    image_url = "nginx:latest"
+    container_name   = "nginx"
+    log_group_region = "${var.aws_region}"
+    log_group_name   = "${aws_cloudwatch_log_group.app.name}"
+  }
+}
+
+resource "aws_ecs_task_definition" "ghost" {
+  family                = "tf_example_ghost_td"
+  container_definitions = "${data.template_file.task_definition.rendered}"
+}
+
+resource "aws_ecs_service" "test" {
+  name            = "tf-example-ecs-ghost"
+  cluster         = "${aws_ecs_cluster.main.id}"
+  task_definition = "${aws_ecs_task_definition.ghost.arn}"
+  desired_count   = 1
+  iam_role        = "${aws_iam_role.ecs_service.name}"
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.test.id}"
+    container_name   = "nginx"
+    container_port   = "80"
+  }
+
+  depends_on = [
+    "aws_iam_role_policy.ecs_service",
+    "aws_alb_listener.front_end",
+  ]
+}
+
+## IAM
+
+resource "aws_iam_role" "ecs_service" {
+  name = "tf_example_ecs_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_service" {
+  name = "tf_example_ecs_policy"
+  role = "${aws_iam_role.ecs_service.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:Describe*",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:RegisterTargets"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "app" {
+  name  = "tf-ecs-instprofile"
+  role = "${aws_iam_role.app_instance.name}"
+}
+
+resource "aws_iam_role" "app_instance" {
+  name = "tf-ecs-example-instance-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+data "template_file" "instance_profile" {
+  template = "${file("${path.module}/instance-profile-policy.json")}"
+
+  vars {
+    app_log_group_arn = "${aws_cloudwatch_log_group.app.arn}"
+    ecs_log_group_arn = "${aws_cloudwatch_log_group.ecs.arn}"
+  }
+}
+
+resource "aws_iam_role_policy" "instance" {
+  name   = "TfEcsExampleInstanceRole"
+  role   = "${aws_iam_role.app_instance.name}"
+  policy = "${data.template_file.instance_profile.rendered}"
+}
+
+## ALB
+
+resource "aws_alb_target_group" "test" {
+  name     = "tf-example-ecs-ghost"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+}
+
+resource "aws_alb" "main" {
+  name            = "tf-example-alb-ecs"
+  subnets         = ["${aws_subnet.main.*.id}"]
+  security_groups = ["${aws_security_group.lb_sg.id}"]
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.test.id}"
+    type             = "forward"
+  }
+}
+
+## CloudWatch Logs
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = "tf-ecs-group/ecs-agent"
+}
+
+resource "aws_cloudwatch_log_group" "app" {
+  name = "tf-ecs-group/app-ghost"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "elb.address" {
-  value = "${aws_elb.web.dns_name}"
+output "instance_security_group" {
+  value = "${aws_security_group.instance_sg.id}"
 }
 
-output "ec2.public_ip" {
-  value = ["${aws_instance.web.*.public_ip}"]
+output "launch_configuration" {
+  value = "${aws_launch_configuration.app.id}"
+}
+
+output "asg_name" {
+  value = "${aws_autoscaling_group.app.id}"
+}
+
+output "elb_hostname" {
+  value = "${aws_alb.main.dns_name}"
 }
