abdd26fbf52bcab7c6c1f965d8a4863024704938
Eric Dahl <eric.c.dahl@gmail.com>
2017-04-30 20:43:35 -0700

    add app controller

---
+++ app/src/main/java/example/endpoints/AppController.java
@@ -0,0 +1,18 @@
+package example.endpoints;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @Value("${HOSTNAME}")
+    private String hostname;
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String index() {
+        return "Hello from " + hostname;
+    }
+}
