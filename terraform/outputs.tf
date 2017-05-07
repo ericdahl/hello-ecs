f806dd8c67394b44872707dbab9a49e50e60a870
Eric Dahl <eric.c.dahl@gmail.com>
2017-05-07 11:28:47 -0700

    add tags/outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,7 @@ output "asg_name" {
 output "elb_hostname" {
   value = "${aws_alb.main.dns_name}"
 }
+
+output "vpc_subnets" {
+  value = ["${aws_subnet.main.*.cidr_block}"]
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,6 +6,10 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.10.0.0/16"
+
+  tags {
+    Name = "terraform"
+  }
 }
 
 resource "aws_subnet" "main" {
@@ -13,6 +17,10 @@ resource "aws_subnet" "main" {
   cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "tf-main"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
