c6da4354dd29109b83cc61bbf2799093a3a76419
Eric Dahl <eric.c.dahl@gmail.com>
2023-03-05 10:00:13 -0800

    cleanup outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "alb_hostname" {
-  value = aws_alb.default.dns_name
-}
-
-output "vpc_subnets" {
-  value = [aws_subnet.public.*.cidr_block]
+output "alb" {
+  value = "http://${aws_alb.default.dns_name}"
 }
